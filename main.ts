4594470c0ad6d04dca72cef8a3a36e2ab3269b92
yus840332 <70998708+yus840332@users.noreply.github.com>
2020-10-07 14:54:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,13 +17,29 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(1)
     radio.sendString("number")
 })
+function 前進 () {
+    while (bitbot.sonar(BBPingUnit.Centimeters) > 10) {
+        bitbot.go(BBDirection.Forward, 80)
+        basic.showNumber(1)
+        if (bitbot.sonar(BBPingUnit.Centimeters) < 10) {
+            bitbot.stop(BBStopMode.Coast)
+            basic.showNumber(2)
+            break;
+        }
+        bitbot.stop(BBStopMode.Brake)
+        basic.showNumber(3)
+    }
+}
 input.onGesture(Gesture.LogoDown, function () {
     radio.sendString("F")
     basic.showArrow(ArrowNames.North)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("D")
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "F") {
-        bitbot.go(BBDirection.Forward, 80)
+        前進()
         bitbot.ledClear()
         bitbot.setLedColor(0xFF0000)
     } else if (receivedString == "B") {
@@ -41,6 +57,14 @@ radio.onReceivedString(function (receivedString) {
     } else if (receivedString == "S") {
         bitbot.stop(BBStopMode.Coast)
         bitbot.ledClear()
+    } else if (receivedString == "D") {
+        for (let index = 0; index < 4; index++) {
+            bitbot.buzz(true)
+            basic.pause(100)
+            bitbot.buzz(false)
+            basic.pause(0.000000000000000000000001)
+        }
+        bitbot.buzz(false)
     } else {
         bitbot.stop(BBStopMode.Coast)
         bitbot.ledClear()
